9af9bd3a7814b37bb044c7a61985d90cdea33d0c
tema <sezemov2@gmail.com>
2026-03-29 13:17:25 +0300

    feat: add locks in index storage to provide multithreading

--- src/main/java/org/example/library/core/InMemoryIndex.java
+++ src/main/java/org/example/library/core/InMemoryIndex.java
@@ -4,33 +4,64 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class InMemoryIndex implements IndexStorage {
     // token - paths
     private ConcurrentMap<String, Set<String>> index = new ConcurrentHashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
 
     public void addToken(String token, Set<String> paths) {
-        index.put(token, paths);
+        writeLock.lock();
+        try {
+            index.put(token, paths);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void updateToken(String token, Set<String> paths) {
-        Set<String> newPaths = index.get(token);
-        newPaths.addAll(paths);
-        index.put(token, newPaths);
+        writeLock.lock();
+        try {
+            Set<String> newPaths = index.get(token);
+            newPaths.addAll(paths);
+            index.put(token, newPaths);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void removeToken(String token) {
-        index.remove(token);
+        writeLock.lock();
+        try {
+            index.remove(token);
+        } finally {
+            writeLock.lock();
+        }
     }
 
     public void removePath(String path) {
-        index.forEach((token, paths) -> {
-            paths.remove(path);
-        });
+        writeLock.lock();
+        try {
+            index.forEach((token, paths) -> {
+                paths.remove(path);
+            });
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public Set<String> search(String token) {
-        return index.getOrDefault(token, Collections.emptySet());
+        readLock.lock();
+        try {
+            return index.getOrDefault(token, Collections.emptySet());
+        } finally {
+            readLock.unlock();
+        }
     }
 
 }
