14f92d9a20d05f799d651e50738c22017b13ea17
tema <sezemov2@gmail.com>
2026-03-29 12:57:57 +0300

    fix: changed constructor annotation

--- src/main/java/org/example/library/tokenizator/RegexTokenizer.java
+++ src/main/java/org/example/library/tokenizator/RegexTokenizer.java
@@ -1,10 +1,11 @@
 package org.example.library.tokenizator;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RegexTokenizer implements Tokenizer{
     private final String regex;
 
